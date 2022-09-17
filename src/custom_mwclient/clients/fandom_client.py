90ddf0b8a59dd35fed00f82a947acd36d4eb3e68
h9a-rD4ubXs8 <113800212+h9a-rD4ubXs8@users.noreply.github.com>
2022-09-18 01:26:42 +0200

    Implement get_current_wiki_name

--- src/custom_mwclient/clients/fandom_client.py
+++ src/custom_mwclient/clients/fandom_client.py
@@ -6,3 +6,23 @@ class FandomClient(WikiClient):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+
+    def get_current_wiki_name(self) -> str:
+        """Return the name of the current host.
+
+        The `.fandom.com` part is omitted and `/<lang>` is appended, if the
+        language is not English.
+        """
+
+        api_result = self.api('query', meta='siteinfo', siprop='general')
+        api_result = api_result.get('query', {}).get('general', {})
+
+        sitename = api_result.get('servername', '')
+        sitename = sitename.replace('.fandom.com', '')
+
+        sitelang = api_result.get('lang')
+        if sitelang and sitelang != "en":
+            sitename += '/' + sitelang
+
+        return sitename

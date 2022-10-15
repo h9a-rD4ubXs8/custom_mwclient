7790f3c7e47784b1604365b5fd80ed1ad8428f20
h9a-rD4ubXs8 <113800212+h9a-rD4ubXs8@users.noreply.github.com>
2022-10-15 14:24:35 +0200

    Add ApiContinueError

--- src/custom_mwclient/errors.py
+++ src/custom_mwclient/errors.py
@@ -13,6 +13,20 @@ class RetriedLoginAndStillFailed(AssertUserFailedError):
         return "Tried to re-login but still failed. Attempted action: {}".format(self.action)
 
 
+class ApiContinueError(Exception):
+    """Raised when an exception occurs during an api_continue call."""
+    def __init__(self, loop_index: int, action: str, parameters: dict):
+        self.loop_index = loop_index
+        self.action = action
+        self.parameters = parameters
+
+    def __str__(self):
+        return (
+            f"Error during api_continue call at index {self.loop_index}. "
+            f'Action was "{self.action}", parameters were {self.parameters}.'
+        )
+
+
 class ErrorDuringScriptExecution(BaseException):
     """
     To be raised when a fatal error occurs during script execution that causes the script to terminate.

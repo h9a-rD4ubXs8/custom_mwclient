d9b11d9ac59f13cc73d164cf887178c761069206
h9a-rD4ubXs8 <113800212+h9a-rD4ubXs8@users.noreply.github.com>
2022-10-16 02:25:53 +0200

    Remove unused error classes

--- src/custom_mwclient/errors.py
+++ src/custom_mwclient/errors.py
@@ -1,10 +1,6 @@
 from mwclient.errors import AssertUserFailedError
 
 
-class InvalidUserFile(KeyError):
-    pass
-
-
 class RetriedLoginAndStillFailed(AssertUserFailedError):
     def __init__(self, action):
         self.action = action
@@ -25,24 +21,3 @@ class ApiContinueError(Exception):
             f"Error during api_continue call at index {self.loop_index}. "
             f'Action was "{self.action}", parameters were {self.parameters}.'
         )
-
-
-class ErrorDuringScriptExecution(BaseException):
-    """
-    To be raised when a fatal error occurs during script execution that causes the script to terminate.
-    """
-    pass
-
-
-class ScriptExecutionInvalid(BaseException):
-    """
-    To be raised when the execution of the script becomes invalid while it is still running.
-    """
-    pass
-
-
-class ScriptExecutionKilledByPill(BaseException):
-    """
-    To be raised when the execution of the script is terminated via pill2kill.
-    """
-    pass
